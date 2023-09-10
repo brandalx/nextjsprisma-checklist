fb74b25d9153dd20c38004a827d5ec7848133ec6
brndalx <113251192+brandalx@users.noreply.github.com>
2023-09-10 06:23:08 +0300

    feat: basic layout and linking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <div>
-        <h1>Checklist 1.0</h1>
+      <header className=" flex justify-between mb-4">
+        <h1>Checklist</h1>
+        <Link
+          className=" border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:file:bg-slate-700 outline-none"
+          href="/newlist"
+        >
+          Add new
+        </Link>
+      </header>
+      <div className="pl-4">
+        <ul>Element 1</ul>
       </div>
     </>
   );
